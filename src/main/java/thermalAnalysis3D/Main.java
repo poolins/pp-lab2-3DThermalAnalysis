01e885213aa3820ca006aaaefa25d8410f639bdc
Polina Glezova <pglezova@gmail.com>
2023-09-29 21:57:09 +0300

    added experiment

--- src/main/java/thermalAnalysis3D/Main.java
+++ src/main/java/thermalAnalysis3D/Main.java
@@ -4,18 +4,23 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         int temperature = 10;
         int iterations = 40000;
-        int nodes = 10;
-        int parallel = 3;
 
-        double[][][] initialMatrix = Matrix.generateRandomMatrix(temperature, nodes, nodes, nodes);
-        Solver solver = new Solver(parallel);
-        System.out.println("Number of nodes: " + nodes);
-        System.out.println("Number of threads: " + parallel);
+        int nodes[] = {4, 16};
+        int threads[] = {4, 16, 256, 1024};
 
-        long startTime = System.currentTimeMillis();
-        Solver.calculate(initialMatrix, nodes, nodes, nodes, iterations, temperature);
-        long endTime = System.currentTimeMillis();
-        System.out.printf("Working time = %d milliseconds%n", endTime - startTime);
-        Matrix.print(initialMatrix);
+        for (int i = 0; i < nodes.length; i++) {
+            for (int j = 0; j < threads.length; j++) {
+                double[][][] initialMatrix = Matrix.generateRandomMatrix(temperature, nodes[i], nodes[i], nodes[i]);
+                Solver solver = new Solver(threads[j]);
+                System.out.println("Number of nodes: " + nodes[i]);
+                System.out.println("Number of threads: " + threads[j]);
+
+                long startTime = System.currentTimeMillis();
+                Solver.calculate(initialMatrix, nodes[i], nodes[i], nodes[i], iterations, temperature);
+                long endTime = System.currentTimeMillis();
+                System.out.printf("Working time = %d milliseconds%n", endTime - startTime);
+            }
+        }
+
     }
 }
