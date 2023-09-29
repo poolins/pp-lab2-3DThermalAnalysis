955dc2922c13de13bea24a6eccbd7e8211d98076
Polina Glezova <pglezova@gmail.com>
2023-09-29 22:38:58 +0300

    experiment improved

--- src/main/java/thermalAnalysis3D/Main.java
+++ src/main/java/thermalAnalysis3D/Main.java
@@ -5,22 +5,19 @@ public class Main {
         int temperature = 10;
         int iterations = 40000;
 
-        int nodes[] = {4, 16};
-        int threads[] = {4, 16, 256, 1024};
+        int nodes = 4;
+        int threads[] = {4, 10, 16, 20, 64, 256, 1024};
 
-        for (int i = 0; i < nodes.length; i++) {
-            for (int j = 0; j < threads.length; j++) {
-                double[][][] initialMatrix = Matrix.generateRandomMatrix(temperature, nodes[i], nodes[i], nodes[i]);
-                Solver solver = new Solver(threads[j]);
-                System.out.println("Number of nodes: " + nodes[i]);
-                System.out.println("Number of threads: " + threads[j]);
+        for (int j = 0; j < threads.length; j++) {
+            double[][][] initialMatrix = Matrix.generateRandomMatrix(temperature, nodes, nodes, nodes);
+            Solver solver = new Solver(threads[j]);
+            System.out.println("Number of nodes: " + nodes);
+            System.out.println("Number of threads: " + threads[j]);
 
-                long startTime = System.currentTimeMillis();
-                Solver.calculate(initialMatrix, nodes[i], nodes[i], nodes[i], iterations, temperature);
-                long endTime = System.currentTimeMillis();
-                System.out.printf("Working time = %d milliseconds%n", endTime - startTime);
-            }
+            long startTime = System.currentTimeMillis();
+            Solver.calculate(initialMatrix, nodes, nodes, nodes, iterations, temperature);
+            long endTime = System.currentTimeMillis();
+            System.out.printf("Working time = %d milliseconds%n", endTime - startTime);
         }
-
     }
 }
